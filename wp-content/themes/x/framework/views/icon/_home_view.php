dfcddc569200f2b01ed2b3b7bca9d469bb6dfd00
nitingge@126.com <nitingge@126.com>
2016-02-01 02:01:16 +0800

    ci

--- wp-content/themes/x/framework/views/icon/_home_view.php
+++ wp-content/themes/x/framework/views/icon/_home_view.php
@@ -90,7 +90,64 @@ font-size: 24px;
 font-weight: 400;
 line-height: 29px;">CASE STATUS UPDATE</span>
 </div>
-<?php
+<article style="max-width:1000px;width:90%" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <div class="">
+    <div class="">
+       <span style="font-size: 12px;
+font-weight: 300;
+line-height: 14px;">On&nbsp;&nbsp;Jan 19. 2016 </span>
+       <span style="
+font-size: 14px;
+font-weight: 400;
+line-height: 20px;
+color: #313131;">
+       <p>
+         A motion to dismiss was filed by the defendants in Baltimore City Circuit Court on January 8, 2016.  The motion seeks to dismiss all of the plaintiffs’ claims on the theory that they should have been brought through the administrative process established for tax disputes.  We are preparing a response to the motion that will be filed later this month.
+       </p>
+       </span>
+    </div>
+  </div>
+</article>
+
+<article style="max-width:1000px;width:90%" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <div class="">
+    <div class="">
+       <span style="font-size: 12px;
+font-weight: 300;
+line-height: 14px;">On&nbsp;&nbsp;December 10, 2015 </span>
+       <span style="
+font-size: 14px;
+font-weight: 400;
+line-height: 20px;
+color: #313131;">
+       <p>
+An amended complaint was filed in this case.  Three named plaintiffs were added – Bruce Feinerman and Jeffrey and Arielle Grill.  The named plaintiffs also stated an additional claim against Defendant the Comptroller of the Treasury of Maryland for violation of the dormant Commerce Clause of the United States Constitution.  A responsive pleading must be filed by the defendants by January 10, 2016.
+       </p>
+       </span>
+    </div>
+  </div>
+</article>
+
+<article style="max-width:1000px;width:90%" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <div class="">
+    <div class="">
+       <span style="font-size: 12px;
+font-weight: 300;
+line-height: 14px;">On&nbsp;&nbsp;November 13, 2015 </span>
+       <span style="
+font-size: 14px;
+font-weight: 400;
+line-height: 20px;
+color: #313131;">
+       <p>
+The original complaint was filed was filed in Baltimore City Circuit Court on behalf of Michael Holzheid against the Comptroller of the Treasury of Maryland and the State of Maryland.
+       </p>
+       </span>
+    </div>
+  </div>
+</article>
+
+<!--<?php
 $docPosts = new WP_Query();
 $docPosts->query('cat=4');
 ?>
@@ -111,5 +168,5 @@ color: #313131;">
     </div>
   </div>
 </article>
-<?php endwhile; ?>
+<?php endwhile; ?>-->
 </div>
