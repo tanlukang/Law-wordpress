47fd386d9c0eb991e7e0ae7a003e67e434cf7006
nitingge@126.com <nitingge@126.com>
2016-02-01 21:30:40 +0800

    ci

--- download.php
+++ download.php
@@ -1,7 +1,7 @@
 <?php
 $mysql_server_name="localhost"; //数据库服务器名称
 $mysql_username="root"; // 连接数据库用户名
-$mysql_password=""; // 连接数据库密码
+$mysql_password="1ncpad"; // 连接数据库密码
 $mysql_database="wordpress_avocode"; // 数据库的名字
 
 $conn = mysql_connect($mysql_server_name, $mysql_username,
